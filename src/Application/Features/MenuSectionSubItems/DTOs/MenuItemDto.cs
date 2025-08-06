b00d84c9569650a7095be8c99c559676c16a3173
coderrayhan <rakibrayhan8@gmail.com>
2025-08-07 00:39:51 +0600

    breadcrumb query

---
+++ src/Application/Features/MenuSectionSubItems/DTOs/MenuItemDto.cs
@@ -0,0 +1,24 @@
+﻿namespace CleanArchitecture.Blazor.Application.Features.MenuSectionSubItems.DTOs;
+
+public class MenuItemDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string[] MenuSectionRoles { get; set; }
+    public int MenuSectionSerialNo { get; set; }
+
+    public string MenuSectionItemTitle { get; set; }
+    public string Icon { get; set; }
+    public string MenuSectionItemHref { get; set; }
+    public bool IsParent { get; set; }
+    public PageStatus MenuSectionItemPageStatus { get; set; }
+    public int MenuSectionItemSerialNo { get; set; }
+
+    public int MenuSectionSubItemId { get; set; }
+    public int MenuSectionItemId { get; set; }
+    public string MenuSectionSubItemTitle { get; set; }
+    public string MenuSectionSubItemHref { get; set; }
+    public string[] MenuSectionSubItemRoles { get; set; }
+    public PageStatus MenuSectionSubItemPageStatus { get; set; }
+    public int MenuSectionSubItemSerialNo { get; set; }
+}
---
+++ src/Application/Features/MenuSectionSubItems/Queries/GetBreadcrumbData/GetBreadcrumbDataByHrefQuery.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Blazor.Application.Features.MenuSectionSubItems.DTOs;
+
+namespace CleanArchitecture.Blazor.Application.Features.MenuSectionSubItems.Queries.GetBreadcrumbData;
+
+public class GetBreadcrumbDataByHrefQuery : IRequest<MenuItemForBreadcrumbDto>
+{
+    public string Href { get; set; }
+}
+
+public class GetBreadcrumbDataByHrefQueryHandler
+    : IRequestHandler<GetBreadcrumbDataByHrefQuery, MenuItemForBreadcrumbDto>
+{
+    public Task<MenuItemForBreadcrumbDto> Handle(GetBreadcrumbDataByHrefQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
