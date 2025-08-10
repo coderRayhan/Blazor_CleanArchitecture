797b76e37c11f3e6fcb0c63e4b609e1e31f95ea1
coderrayhan <rakibrayhan8@gmail.com>
2025-08-10 08:16:19 +0600

    common auto complete added

--- src/Application/Common/Constants/SelectListConstants.cs
+++ src/Application/Common/Constants/SelectListConstants.cs
@@ -1,31 +1,31 @@
 ﻿namespace CleanArchitecture.Blazor.Application.Common.Constants;
 public static class SelectListConstants
 {
-    public const string GetMenuSectionSelectList = """
-        SELECT Id, Title
+    public const string GetMenuSectionSelectList = $"""
+        SELECT Id, Title Name
         FROM dbo.MenuSections MS
         WHERE 1 = 1
         """;
 
-    public const string GetMenuSectionItemSelectList = """
-        SELECT Id, Title
+    public const string GetMenuSectionItemSelectList = $"""
+        SELECT Id, Title Name
         FROM dbo.MenuSectionItems MSI
         WHERE 1 = 1
         """;
 
-    public const string GetLookupSelectList = """
+    public const string GetLookupSelectList = $"""
         SELECT Id, Name
         FROM dbo.Lookups 
         WHERE 1 = 1
         """;
     
-    public const string GetLookupDetailSelectList = """
+    public const string GetLookupDetailSelectList = $"""
         SELECT Id, Name
         FROM dbo.LookupDetails 
         WHERE 1 = 1
         """;
 
-    public const string GetLookupDetailSelectListByLookupId = """
+    public const string GetLookupDetailSelectListByLookupId = $"""
         SELECT Id, Name 
         FROM dbo.LookupDetails
         WHERE 1 = 1
