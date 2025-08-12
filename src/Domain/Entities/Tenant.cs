4f7bfb73c2726c32f46566ecf9052b9e0659d14f
coderrayhan07 <rakibrayhan8@gmail.com>
2025-08-12 18:03:58 +0600

    tenant entity

---
+++ src/Domain/Entities/Tenant.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Blazor.Domain.Common.Entities;
+
+namespace CleanArchitecture.Blazor.Domain.Entities;
+internal class Tenant : IEntity<string>
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string? Name { get; set; }
+    public string? ShortCode { get; set; }
+    public string? HeaderText { get; set; }
+    public string? Description { get; set; }
+}
